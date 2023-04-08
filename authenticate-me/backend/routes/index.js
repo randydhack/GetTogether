b5d580111e9b03d2b18c94d557ff2b2ba81892e2
randy hac <randydhacks@gmail.com>
2023-04-08 10:08:33 -0700

    phase 1 done

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,6 +1,8 @@
 // backend/routes/index.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+
+const apiRouter = require("./api");
 
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
@@ -9,11 +11,13 @@ const router = express.Router();
 
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    "XSRF-Token": csrfToken,
   });
+});
+
+router.use("/api", apiRouter);
 
 module.exports = router;
